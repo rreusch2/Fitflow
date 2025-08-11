fa82345580496f18408b4ce2cf2fe3209d90008b
rreusch2 <reid.reusch@gmail.com>
2025-08-11 06:07:33 -0500

    newww

---
+++ Nexus/Nexus/NexusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexusApp.swift
+//  Nexus
+//
+//  Created by Reid Reusch on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct NexusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Nexus/NexusTests/NexusTests.swift
@@ -0,0 +1,17 @@
+//
+//  NexusTests.swift
+//  NexusTests
+//
+//  Created by Reid Reusch on 8/11/25.
+//
+
+import Testing
+@testable import Nexus
+
+struct NexusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
